84d8c1e0ca16372097f7624beb62104d167e7345
HatemAlawwad <hatem.alawwad@gmail.com>
2025-08-19 14:21:41 +0300

    add Vendor

---
+++ src/main/java/com/spring/boot/springbootcapstone3/Model/Vendor.java
@@ -0,0 +1,51 @@
+package com.spring.boot.springbootcapstone3.Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Check;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Check(constraints = "approval_status IN ('PENDING','APPROVED','REJECTED','SUSPENDED')")
+@Table(name = "vendor",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email"),
+                @UniqueConstraint(columnNames = "phone")
+        })
+public class Vendor {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty @Size(min = 3, max = 100)
+    @Column(columnDefinition = "varchar(100) not null")
+    private String name;
+
+    @NotEmpty @Email
+    @Column(columnDefinition = "varchar(120) not null", unique = true)
+    private String email;
+
+    @NotEmpty @Size(min = 7, max = 20)
+    @Pattern(regexp = "^[0-9+()\\-\\s]+$", message = "Phone contains invalid characters")
+    @Column(columnDefinition = "varchar(20) not null", unique = true)
+    private String phone;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column(name = "approval_status",columnDefinition = "varchar(20) not null default 'PENDING'")
+    @Pattern(
+            regexp = "^(PENDING|APPROVED|REJECTED|SUSPENDED)$",
+            message = "Status must be PENDING, APPROVED, REJECTED, or SUSPENDED")
+    private String approvalStatus = "PENDING";
+
+}
