bbbbecdbe5ea006f102fdd265239935ca9fb0cdf
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-08-23 06:22:04 +0300

    extra endpoints #9

---
+++ src/main/java/com/spring/boot/springbootcapstone3/DTO/ContractsStatisticsOutDTO.java
@@ -0,0 +1,18 @@
+package com.spring.boot.springbootcapstone3.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContractsStatisticsOutDTO {
+    private Double totalActiveContracts;
+    private Double totalExpiringSoonContracts;
+    private Double totalExpiredContracts;
+    private Double totalPaidRevenue;
+
+}
